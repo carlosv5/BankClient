a69e873e189a7c02b349301762d6a066190dbc5d
nachoigs <nachoperegrino94@gmail.com>
2017-11-27 13:22:33 +0100

    Update TCPServer.java

--- BankClient/src/es/upm/dit/cnvr/server/TCPServer.java
+++ BankClient/src/es/upm/dit/cnvr/server/TCPServer.java
@@ -18,7 +18,7 @@ public class TCPServer {
         while (true) {
             try {
                 connectionSocket = welcomeSocket.accept();
-
+                // TODO: NO CREAR CLIENTDB AQUI, inicializarlo antes y meterlo en la llamada.
                 ConnectionDispatcher conHandler = new ConnectionDispatcher(connectionSocket, id, new ClientDB());
                 System.out.println("Get a socket connection");
                 conHandler.start();
